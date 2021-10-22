953921c6fb616f1dc8638ca183b092a6f372fa46
Roei Aviad <roeiav24@gmail.com>
2021-10-22 21:28:24 +0300

    added logo.

--- kernel/kernel_main.c
+++ kernel/kernel_main.c
@@ -1,11 +1,15 @@
 #include "../libc/screen.h"
 #include <stdint.h>
 
+/*
+	print ROSH logo
+*/
+void printLogo();
+
 void main() {
-	char welcome[] = "Welcome to ROSH!\n";
+	printLogo();
 	char numstr[] = "This is a number:\t";
 	int num = 2265;
-	puts(welcome);
 	puts(numstr);
 	puti(num);
 	putc('\n');
@@ -13,4 +17,15 @@ void main() {
 	puts("\rLOL");
 
 	while (1) {}
+}
+
+void printLogo()
+{
+	char logo[] = "\t\t\t\t\t\t\t______ _____ _____ _   _ \n"
+				"\t\t\t\t\t\t\t| ___ \\  _  /  ___| | | |\n"
+				"\t\t\t\t\t\t\t| |_/ / | | \\ `--.| |_| |\n"
+				"\t\t\t\t\t\t\t|    /| | | |`--. \\  _  |\n"
+				"\t\t\t\t\t\t\t| |\\ \\\\ \\_/ /\\__/ / | | |\n"
+				"\t\t\t\t\t\t\t\\_| \\_|\\___/\\____/\\_| |_/\n\n\n";
+	puts(logo);
 }
