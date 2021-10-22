4a2724c6dae5db133c601fcefb618f7ff578c4c7
Roei Aviad <roeiav24@gmail.com>
2021-10-22 18:04:18 +0300

    strlen function added.

---
+++ libc/string.c
@@ -0,0 +1,9 @@
+#include "string.h"
+
+int strlen(char* str)
+{
+	int i = 0;
+	while (str != 0)	// while not end
+		i++;	// continue
+	return i;		// return length on end
+}
---
+++ libc/string.h
@@ -0,0 +1,11 @@
+#ifndef STRINGH
+#define STRINGH
+
+/*
+	get length of str
+	Input: char array
+	Output: str length
+*/
+int strlen(char* str);
+
+#endif
