cfb392d643efcb33b4d9a1df391e7adb3576a749
Roei Aviad <roeiav24@gmail.com>
2021-10-22 13:41:27 +0300

    print string function added to boot.

---
+++ boot/boot_print.s
@@ -0,0 +1,15 @@
+; print a string using BIOS interrupt
+; Input: bx - string address
+boot_print:
+	mov al, [bx]	; get char
+	cmp al, 0		; if end string done
+	je print_end
+	
+	mov ah, 0x0e	; print char
+	int 0x10
+	
+	inc bx			; next char
+	jmp boot_print
+
+	print_end:
+		ret
