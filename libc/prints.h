8469d7865a37018a0c221343672c87ffa5ffd5c8
Roei Aviad <roeiav24@gmail.com>
2021-10-22 15:36:36 +0300

    prints h file created.

---
+++ libc/prints.h
@@ -0,0 +1,16 @@
+#ifndef PRINTSH
+#define PRINTSH
+
+/*
+	print a char to the screen
+	Input: the char to print
+*/
+void putc(char c);
+
+/*
+	print a string to the screen
+	Input: the strint to print (ends with \0)
+*/
+void puts(char* str);
+
+#endif
