1200dfa797899b34cc7c34088430b287649466c7
Roei Aviad <roeiav24@gmail.com>
2021-10-30 15:01:09 +0300

    16 irq handlers added.

---
+++ kernel/IDT/irq_handlers.s
@@ -0,0 +1,79 @@
+irq0:
+	push byte 0
+	push byte 32
+	jmp irq_main
+
+irq1:
+	push byte 1
+	push byte 33
+	jmp irq_main
+
+irq2:
+	push byte 2
+	push byte 34
+	jmp irq_main
+
+irq3:
+	push byte 3
+	push byte 35
+	jmp irq_main
+
+irq4:
+	push byte 4
+	push byte 36
+	jmp irq_main
+
+irq5:
+	push byte 5
+	push byte 37
+	jmp irq_main
+
+irq6:
+	push byte 6
+	push byte 38
+	jmp irq_main
+
+irq7:
+	push byte 7
+	push byte 39
+	jmp irq_main
+
+irq8:
+	push byte 8
+	push byte 40
+	jmp irq_main
+
+irq9:
+	push byte 9
+	push byte 41
+	jmp irq_main
+
+irq10:
+	push byte 10
+	push byte 42
+	jmp irq_main
+
+irq11:
+	push byte 11
+	push byte 43
+	jmp irq_main
+
+irq12:
+	push byte 12
+	push byte 44
+	jmp irq_main
+
+irq13:
+	push byte 13
+	push byte 45
+	jmp irq_main
+
+irq14:
+	push byte 14
+	push byte 46
+	jmp irq_main
+
+irq15:
+	push byte 15
+	push byte 47
+	jmp irq_main
