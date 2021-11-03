e36524df32661ae57d1772c093abf5bd6b7af7b1
Yoav Shaham <yoavsh2004@gmail.com>
2021-11-03 18:57:50 +0200

    keyboard_putc added

--- kernel/IDT/idt.c
+++ kernel/IDT/idt.c
@@ -76,8 +76,8 @@ void idt_gate_initialize(uint32_t gate_num,
  uint16_t selector, 
  uint8_t flags)
  {
-     g_idt_gates[gate_num].base_low = gate_num & 0xFFFF;    //getting low 16 bits
-     g_idt_gates[gate_num].base_high = (gate_num >> 16) & 0xFFFF;   //getting high 16 bits
+     g_idt_gates[gate_num].base_low = base & 0xFFFF;    //getting low 16 bits
+     g_idt_gates[gate_num].base_high = (base >> 16) & 0xFFFF;   //getting high 16 bits
      g_idt_gates[gate_num].zero = 0;
      g_idt_gates[gate_num].selector = selector;
      g_idt_gates[gate_num].flags = flags;
---
+++ kernel/IDT/keyboard.h
@@ -0,0 +1,48 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <stdint.h>
+#include "../ports.h"
+
+#define KEYBOARD_INPUT_PORT 0X60
+#define KEYBOARD_OUTPUT_PORT 0x20
+
+#define UNKNOWN_SYMBOL 0
+#define NUM_OF_SYMBOLS 256
+
+#define ESCAPE_SYMBOL       0x1B
+#define HOME_SYMBOL        0xE0
+#define END_SYMBOL         0xE1
+#define UP_SYMBOL          0xE2
+#define DOWN_SYMBOL        0xE3
+#define LEFT_SYMBOL        0xE4
+#define RIGHT_SYMBOL       0xE5
+#define PAGEUP_SYMBOL      0xE6
+#define PAGEDOWN_SYMBOL    0xE7
+#define INSERT_SYMBOL      0xE8
+#define DELETE_SYMBOL      0xE9
+
+/*
+    This function handels keyboard input 
+*/
+void keyboard_handler();
+
+/*
+    This function parses keyboard input symbol into ascii
+    input_symbol: keyboard symbol
+    return: ascii representation 
+*/
+uint8_t patse_to_ascii(uint8_t input_symbol);
+
+/*
+    This function initializes the keyboard symbol array
+*/
+void keyboard_initialize();
+
+/*
+    This function checks whether the char is printable and prints it
+    input_char: the charater that will be checked and printed
+*/
+void keyboard_putc(uint8_t input_char);
+
+#endif
