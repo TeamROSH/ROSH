a707cc12d1fe95cdcaa87bd688ee6024c313d604
Roei Aviad <roeiav24@gmail.com>
2021-11-26 10:33:13 +0200

    gdt_entry_bits struct added.

--- kernel/GDT/gdt.h
+++ kernel/GDT/gdt.h
@@ -21,6 +21,28 @@ uint32_t base;  //first gdt entry
 }__attribute__((packed)) gdt_pointer;
 
 /*
+	GDT entry detailed struct
+	Same size as gdt_entry
+*/
+typedef struct gdt_entry_bits {
+	unsigned int limit_low              : 16;
+	unsigned int base_low               : 24;
+	unsigned int accessed               :  1;
+	unsigned int read_write             :  1;
+	unsigned int conforming_expand_down :  1;
+	unsigned int code                   :  1; // 1 for code, 0 for data
+	unsigned int code_data_segment      :  1; // (is not TSS or LDT?)
+	unsigned int DPL                    :  2; // privilege level
+	unsigned int present                :  1;
+	unsigned int limit_high             :  4;
+	unsigned int available              :  1;
+	unsigned int long_mode              :  1;
+	unsigned int big                    :  1;
+	unsigned int gran                   :  1; // 1 for 4k page addressing, 0 for byte addressing
+	unsigned int base_high              :  8;
+} __attribute__((packed)) gdt_entry_bits;
+
+/*
     This function initaize the gdt
 */
 void gdt_initialize();
