099473cdaba2ecafda369597b9031b526a9ab9f2
Roei Aviad <roeiav24@gmail.com>
2021-11-26 15:31:20 +0200

    syscall function added.

--- kernel/syscalls/syscalls.h
+++ kernel/syscalls/syscalls.h
@@ -1,6 +1,14 @@
 #ifndef SYSCALLS_H
 #define SYSCALLS_H
+#include <stdint.h>
 
-
+/*
+	Syscaller for usermode
+	@param group: group of functions
+	@param function: function number in the group
+	@param params: parameters to pass to the system call
+	@param n: number of parameters
+*/
+void syscall(uint16_t group, uint16_t function, uint32_t* params, int n);
 
 #endif
