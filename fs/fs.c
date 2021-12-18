0cba964cd5a9a0efd0e93f766c4066a4400d4316
Roei Aviad <roeiav24@gmail.com>
2021-12-18 19:52:11 +0200

    fs create file and folder basic added.

--- fs/fs.c
+++ fs/fs.c
@@ -13,4 +13,73 @@ void init_fs()
 	superblock->block_bitmap = (void*)(superblock->inode_bitmap + superblock->inodes_num / 8);
 	superblock->inodes = (void*)(superblock->block_bitmap + superblock->blocks_num / 8);
 	superblock->blocks = (void*)(superblock->inodes + superblock->inodes_num * superblock->inode_size);
+
+	for (int i = 0; i < superblock->inodes_num / 8; i++)
+		superblock->inode_bitmap[i] = 0;
+	for (int i = 0; i < superblock->blocks_num / 8; i++)
+		superblock->block_bitmap[i] = 0;
+}
+
+int take_inode()
+{
+	int found = -1;
+	for (int i = 0; i < superblock->inodes_num; i++)
+	{
+		uint8_t bit = superblock->inodes_num[i / 8] >> (i % 8);
+		if (bit % 2 == 0)
+		{
+			found = i;
+			uint8_t temp = 1;
+			temp >>= (i % 8);
+			superblock->inodes_num[i / 8] |= temp;
+		}
+	}
+	return found;
+}
+
+int take_block()
+{
+	int found = -1;
+	for (int i = 0; i < superblock->blocks_num; i++)
+	{
+		uint8_t bit = superblock->blocks_num[i / 8] >> (i % 8);
+		if (bit % 2 == 0)
+		{
+			found = i;
+			uint8_t temp = 1;
+			temp >>= (i % 8);
+			superblock->blocks_num[i / 8] |= temp;
+		}
+	}
+	return found;
+}
+
+void create_folder()
+{
+	int inode_num = take_inode();
+	if (inode_num == -1)
+		return;
+	Inode* inode = (Inode*)(superblock->inodes + inode_num);
+	inode->folder = 1;
+	inode->size = 0;
+	
+	int block_num = take_block();
+	if (block_num == -1)
+		return;
+	inode->block = (Block*)(superblock->blocks + block_num);
+}
+
+void create_file(char* name)
+{
+	int inode_num = take_inode();
+	if (inode_num == -1)
+		return;
+	Inode* inode = (Inode*)(superblock->inodes + inode_num);
+	inode->folder = 0;
+	inode->size = 0;
+	
+	int block_num = take_block();
+	if (block_num == -1)
+		return;
+	inode->block = (Block*)(superblock->blocks + block_num);
 }
--- fs/fs.h
+++ fs/fs.h
@@ -3,29 +3,43 @@
 
 #include <stdint.h>
 
+typedef struct Block
+{
+	void data[512];
+} Block;
+
 typedef struct Inode
 {
-	int8_t folder : 1;	// 0 - file, 1 - folder
-	int32_t size;		// file size
-	void* block;
+	uint8_t folder : 1;	// 0 - file, 1 - folder
+	uint32_t size;		// file size
+	Block* block;
 } Inode;
-
 
 typedef struct Superblock
 {
 	void* inode_bitmap;
 	void* block_bitmap;
-	void* inodes;
-	void* blocks;
-	int32_t inodes_num;
-	int32_t blocks_num;
-	int32_t inode_size;
-	int32_t block_size;
+	Inode* inodes;
+	Block* blocks;
+	uint32_t inodes_num;
+	uint32_t blocks_num;
+	uint32_t inode_size;
+	uint32_t block_size;
 } Superblock;
 
 /*
 	init a temporary file system
 */
 void* init_fs();
+
+/*
+	create a folder
+*/
+void create_folder(char* path);
+
+/*
+	create a file
+*/
+void create_file(char* name);
 
 #endif
