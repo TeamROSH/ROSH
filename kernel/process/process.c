c676d2b4dbc0edb4e1d10faa9406303f13681e9e
Yoav Shaham <yoavsh2004@gmail.com>
2021-12-19 13:59:35 +0200

    initialize_process_regs added

--- kernel/process/process.c
+++ kernel/process/process.c
@@ -1,20 +1,61 @@
 #include "process.h"
 
-int g_gighest_pid = 1;
+extern Heap g_kernelHeap;
+extern page_directory* g_page_directory;
 
+int g_highest_pid = 1;
+node* g_curr_process;
+
+// holds the current process memory base for process memory allocations
+uint32_t g_curr_process_base; 
+
+process_context_block* create_process(int is_kernel);
 int generate_pid();
+void initialize_process_regs(process_context_block* pcb);
 
+
+process_context_block* create_process(int is_kernel, char[PROCESS_NAME] process_name)
+{
+    process_context_block* pcb = heap_malloc(&g_kernelHeap, sizeof(process_context_block));
+
+    pcb->is_kernel = is_kernel;
+    pcb->pid = generate_pid();
+    memcpy(pcb->name, process_name, PROCESS_NAME);
+    pcb->process_base = g_curr_process;
+    pcb->process_top = g_curr_process + PAGE_SIZE * 20;
+    pcb->reg.esp = 0x100000;
+    
+    //mapping stack
+    pcb->stack = 0x100000;
+    uint32_t* stackPhy = page_to_address(page_alloc());
+    page_map(g_page_directory, stack, stackPhy, PAGE_FLAG_READWRITE | PAGE_FLAG_EXISTS | is_kernel);
+
+    pcb->process_state = PROCESS_CREATED;
+
+    return pcb;
+}
 
 int generate_pid()
 {
     // if there is no more space for more processes 
-    if(g_gighest_pid == MAX_PROCESS)
+    if(g_highest_pid == MAX_PROCESS)
     {
         return NULL;
     }
 
     // inc the pid counter
-    g_gighest_pid++;
+    g_highest_pid++;
 
-    retrun g_gighest_pid - 1;
+    retrun g_highest_pid - 1;
+}
+
+void initialize_process_regs(process_context_block* pcb)
+{
+    pcb->reg.eax = 0;
+    pcb->reg.ebx = 0;
+    pcb->reg.ecx = 0;
+    pcb->reg.edx = 0;
+    pcb->reg.esi = 0;
+    pcb->reg.edi = 0;
+    pcb->reg.eflags = 518;
 }
--- kernel/process/process.h
+++ kernel/process/process.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include "../IDT/reg_def.h"
 #include "../memory/paging.h"
+#include "../../libc/list.h"
+#include "../../libc/memory.h"
 
 #define PROCESS_NAME 512
 
@@ -14,6 +16,7 @@
 #define PROCESS_DEAD 8      
 #define PROCESS_ZOMBI 16    //idle
 
+#define MAX_PROCESS_PAGES 20
 #define MAX_PROCESS 1024    // there is a limited amount of pages
 
 
@@ -23,18 +26,31 @@ typedef struct process_context_block{
     registers_t reg;                // curr process reg values
     uint32_t process_state;          // the process state
     uint8_t is_kernel;              // kernel or user process
-    page_table* pt                  // the process page table
-    uint32_t* stack                 // stack base pointer
-    uint32_t* stack_top             // stack top pointer
-    uint32_t* data                  // data pointer
-    uint32_t data_size              // data size
-    uint32_t  stack_size            //  curr stack size
+    page_table* pt;                  // the process page table
+    uint32_t memory_pages[MAX_PROCESS_PAGES];
+    uint32_t process_base;
+    uint32_t process_top;
+    uint32_t stack; 
     }process_context_block;
 
 #endif
+
+/*
+    This function creates a process
+    @param is_kernel: 1 if kernel 0 if user
+    @returns the pcb of the new process 
+*/
+process_context_block* create_process(int is_kernel);
 
 /*
     This function generates pid
     @returns the new pid
 */
 int generate_pid();
+
+
+/*
+    This function initializes the pocess context block registes
+    @param pcb: process_context_block pointer
+*/
+void initialize_process_regs(process_context_block* pcb);
