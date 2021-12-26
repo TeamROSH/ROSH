9bcaf789045e3505ea3b80d11c6c4700e90c4051
Yoav Shaham <yoavsh2004@gmail.com>
2021-12-26 12:41:57 +0200

    elf structs added

---
+++ kernel/memory/elf.h
@@ -0,0 +1,33 @@
+#ifndef ELF_H
+#define ELF_H
+
+typedef struct elf_header{
+  uint8_t e_ident[4]; 
+  uint16_t e_type;
+  uint16_t e_machine;
+  uint32_t e_version;
+  uint32_t e_entry;
+  uint32_t e_phoff; 
+  uint32_t e_shoff;
+  uint32_t e_flags;
+  uint16_t e_ehsize;
+  uint16_t e_phentsize; 
+  uint16_t e_phnum; 
+  uint16_t e_shentsize;
+  uint16_t e_shnum;
+  uint16_t e_shstrndx;
+} elf_header;
+ 
+typedef struct program_header {
+  uint32_t p_type;
+  uint32_t p_offset; 
+  uint32_t p_vaddr; 
+  uint32_t p_paddr;
+  uint32_t p_filesz; 
+  uint32_t p_memsz;
+  uint32_t p_flags;
+  uint32_t p_align; 
+} program_header;
+
+
+#endif
