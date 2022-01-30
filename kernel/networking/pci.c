edb7dbf3a7f3b2ae8b81dac1e52d216a808b2915
YoavShaham1 <yoavsh2004@gmail.com>
2022-01-30 07:30:47 -0800

    get_pci_device_data added

--- kernel/networking/pci.c
+++ kernel/networking/pci.c
@@ -1,6 +1,7 @@
 #include "pci.h"
 
 uint16_t read_dword_from_pci(uint8_t bus, uint8_t device, uint8_t func, uint8_t register_ofset);
+pci_header_data* get_pci_device_data(uint8_t bus, uint8_t device);
 
 uint16_t read_dword_from_pci(uint8_t bus, uint8_t device, uint8_t func, uint8_t register_ofset)
 {
@@ -15,4 +16,29 @@ uint16_t read_dword_from_pci(uint8_t bus, uint8_t device, uint8_t func, uint8_t 
 
     // returning 16 bit result
     return (indw(CONFIG_DATA) >> ((register_ofset & 2) * 8)) & 0xFFFF;    
+}
+
+pci_header_data* get_pci_device_data(uint8_t bus, uint8_t device)
+{
+    // allocating space for the pci device data
+    pci_header_data* header = (pci_header_data*) kmalloc(sizeof(pci_header_data));
+
+    // if device is valid
+    if ((header->vendor_id = read_dword_from_pci(bus, device, 0, 0)) == 0xFFFF) {
+        return NULL;
+    } 
+
+    // reading the pci header into the struct
+    header->device_id = read_dword_from_pci(bus, device, 0, 2);
+    header->command = read_dword_from_pci(bus, device, 1, 4);
+    header->status = read_dword_from_pci(bus, device, 1, 6);
+    header->revision_id = read_dword_from_pci(bus, device, 2, 8) & 0xFF;
+    header->prog_if = read_dword_from_pci(bus, device, 2, 8) >> 8;
+    header->subclass = read_dword_from_pci(bus, device, 2, 10) & 0xFF;
+    header->class_code = read_dword_from_pci(bus, device, 2, 10) >> 8;
+    header->cache_line_size = read_dword_from_pci(bus, device, 3, 0XC) & 0xFF;
+    header->latency_timer = read_dword_from_pci(bus, device, 3, 0XC) >> 8;
+    header->header_type = read_dword_from_pci(bus, device, 3, 0XE) & 0xFF;
+    header->bist = read_dword_from_pci(bus, device, 3, 0XE) >> 8;
+    return header;   
 }
--- kernel/networking/pci.h
+++ kernel/networking/pci.h
@@ -3,18 +3,47 @@
 
 #include <stdint.h>
 #include "../ports/ports.h"
+#include "../memory/heap.h"
 
 // physical address of io ports of pci
 #define CONFIG_ADDRESS 0XCF8
 #define CONFIG_DATA 0XCFC
 
+#ifndef NULL
+#define NULL 0
+#endif
+
+
+typedef struct pci_header_data{
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint8_t revision_id;
+    uint8_t prog_if;
+    uint8_t subclass;
+    uint8_t class_code;
+    uint8_t cache_line_size;
+    uint8_t latency_timer;
+    uint8_t header_type;
+    uint8_t bist;
+}pci_header_data;
 /*
     This function reads a 16 bit value from the pci register
     @param bus: the pci bus number
     @param device: the pci device number
     @param func: the pci device function
     @param regiter_offset: the pci register offset 
+    This function returns the data requested
 */
 uint16_t read_dword_from_pci(uint8_t bus, uint8_t device, uint8_t func, uint8_t register_ofset);
+
+/*
+    This function gets pci header data of pci device
+    @param bus: the pci bus number
+    @param device: the pci device number
+    returns the pci device header data
+*/
+pci_header_data* get_pci_device_data(uint8_t bus, uint8_t device);
 
 #endif
